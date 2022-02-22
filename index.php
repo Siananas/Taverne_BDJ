1353a937748dbe69dc5cff254f79ed884a0f793e
ASUS <ASUS@ORD1N4T3UR-4M3L13>
2022-02-22 09:31:27 +0100

    Test 8 Amélie

--- index.php
+++ index.php
@@ -14,8 +14,10 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
         
     <body>
         <h1> Taverne BDJ </h1>
-        <h2>        Introduction </h2> 
-        
+        <h2> Introduction </h2> 
+        intro à définir<BR/>
+        on peut écrire les in fos là <BR/>
+        voir comment on peut mettre une jolie mise en page
         <?php
         // put your code here
         ?>

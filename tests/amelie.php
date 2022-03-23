8f86b48e1ed609ffa78d3c48290e8bf02b7b4d8e
ASUS <ASUS@ORD1N4T3UR-4M3L13>
2022-03-23 12:29:45 +0100

    Page d'accueil 3.4 Amélie

--- tests/amelie.php
+++ tests/amelie.php
@@ -1,8 +1,10 @@
+<!doctype html>
 <html>
-    <head> <link rel = "stylesheet" href="style.css"> </head>
+    <head> <link rel = "stylesheet" href="style.css"> </head> <!-- permet d'aller chercher les stlyes définis dans style.css -->
+    
     <body>
 
         <p class="Parableu"> test mise en page </p>
 
-
     </body>
+</html>

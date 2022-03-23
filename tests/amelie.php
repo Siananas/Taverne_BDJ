31e56a7bebcd4f0aae8e335c3cec8ec1e6d41d18
ASUS <ASUS@ORD1N4T3UR-4M3L13>
2022-03-23 12:34:41 +0100

    Page d'accueil 3.6 Amélie

--- tests/amelie.php
+++ tests/amelie.php
@@ -1,9 +1,12 @@
 <!doctype html>
 <html>
-    <head> <link rel = "stylesheet" href="style.css"> </head> <!-- permet d'aller chercher les stlyes définis dans style.css -->
+    <head> 
+        <link rel = "stylesheet" href="style.css"> 
+    </head> <!-- permet d'aller chercher les stlyes définis dans style.css -->
     
     <body>
-
+        
+        <h1> Hope </h1>
         <p> test mise en page </p>
 
     </body>

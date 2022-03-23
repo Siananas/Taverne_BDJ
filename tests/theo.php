d70fd55a9258bd8e7b8d92da03964c54063b1f5b
thvel <thvel@LAPTOP-43TL9KUU>
2022-03-23 12:37:10 +0100

    Théo / test lien raclette

--- tests/theo.php
+++ tests/theo.php
@@ -1,7 +1,44 @@
-<?php
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+
+<html>
+    <head> <!-- https://fr.wikipedia.org/wiki/Raclette -->
+        <meta charset="UTF-8"> <!-- encodage pour les accent etc... -->
+        
+        <title>Test de Théo ;) </title>
+        
+         <style>
+        .Parableu {
+        color: blue;
+        }
+        </style>
+        
+    </head>
+
+    <body> <!-- corp -->
+        
+        <h1>TavernBDJ</h1> <!-- en tête -->
+        <nav> <!--création d'un menu navigation-->
+             <ul> <!--//Stocker les differentes puces dans une balise-->
+                 <li> <!--//création d'un nouvelle puce-->
+                     <a href=https://fr.wikipedia.org/wiki/Raclette> <!--//relie a un lien-->
+                         Connexion admin
+                     </a>
+                 </li>
+             
+            <ul>
+        <nav>
+       
+                 
+        <p class="Parableu"> Wesh ici c'est chez Théo </p>
+        <form action="verif.php" METHOD="GET">
+            Saisissez Login : <input name="LeLogin"> <BR/>
+            Saisissez MDP : <input name="LeMDP"> <BR/>
+            
+            <button type="summit">Cliquez ici</button>
+        </form>
+        <?php
+        
+        ?>
+    </body>
+</html>
 

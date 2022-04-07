110484ec4fafe21edfc562c6658120e745a139f7
Le Gall Victor <victor@looping.org>
2022-04-07 12:42:46 +0200

    general root

--- tests/victor.php
+++ tests/victor.php
@@ -2,7 +2,7 @@
 
 <?php
 //Connect to databse
-$conn = mysqli_connect('localhost','Victor','root','taverne_bdj');
+$conn = mysqli_connect('localhost','general','root','taverne_bdj');
 
 //if connection=false
 if(!$conn){
@@ -15,9 +15,9 @@ $sql = 'SELECT * FROM snacks';
 $result = mysqli_query($conn, $sql);
 
 //Fetch the resulting row in an array
-$users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+$snacks = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 //Print the array
-print_r($users);
+print_r($snacks);
 
 ?>

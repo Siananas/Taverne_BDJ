a199dcd84950b414ec41b05511c6cf8f33320814
thvel <thvel@LAPTOP-43TL9KUU>
2022-04-08 08:57:58 +0200

    Théo / test2 / création du fichier

---
+++ tests/theo2.php
@@ -0,0 +1,33 @@
+
+
+<html>
+    <head> <!-- https://fr.wikipedia.org/wiki/Raclette -->
+        <meta charset="UTF-8"> <!-- encodage pour les accent etc... -->
+        <!--<link rel="stylesheet" href="style"/>-->
+        <title>Test de Théo ;) </title>
+        <link rel="stylesheet" href="style_theo.css"/>
+    </head>
+
+    <body> <!-- corp -->
+        
+        <h1 class="Titre">TavernBDJ</h1> <!-- en tête -->
+        <nav class = "menu-nav" > <!--création d'un menu navigation-->
+            <ul> <!--//Stocker les differentes puces dans une balise-->
+                <li class="btn" >  <!--//création d'un nouvelle puce-->
+                     <a href=https://fr.wikipedia.org/wiki/Raclette target="_BLANK">
+                      <!--//relie a un lien, Blank  = ouverture nvl page-->
+                      Connexion admin</a></li> 
+                <li class="btn" ><a href=../tests/theo.php>
+                      Refresh</a></li>
+            <ul>
+        <nav>
+       
+       
+            
+
+        <?php
+        
+        ?>
+    </body>
+</html>
+

0bb4ab3d99f8860914160f03b0374b6cc8aab012
thvel <thvel@LAPTOP-43TL9KUU>
2022-04-08 09:44:03 +0200

    Théo / test2 / test echo

--- tests/theo_dos/theo2.php
+++ tests/theo_dos/theo2.php
@@ -6,30 +6,19 @@
         
     // DATA 
         //Connect to databse
-        include '/conn.php';
+        include('../../conn.php');
 
-        // Write querry for all users
-        $sql = 'SELECT * FROM snacks';
-
-        //Make query and get resuklt
-        $result = mysqli_query($conn, $sql);
-        //Fetch the resulting row in an array
-        $snacks = mysqli_fetch_all($result, MYSQLI_ASSOC);
-        print_r($snacks[1]["nom"]);
-
-        //Print the array
-        print_r($snacks);
         ?>
     
     
     <head> <!-- https://fr.wikipedia.org/wiki/Raclette -->
-        <meta charset="UTF-8"> <!-- encodage pour les accent etc... -->
-        <!--<link rel="stylesheet" href="style"/>-->
-        <title>Test de Théo ;) </title>
+        <meta charset="UTF-8"> 
+        
+        <title>Test2 </title>
         <link rel="stylesheet" href="style_theo_2.css"/>
     </head>
 
-    <body> <!-- corp -->
+    <body>
         
         <h1 class="Titre">TavernBDJ</h1> <!-- en tête -->
        <nav class = "liste" >
@@ -37,8 +26,8 @@
             <?php echo $snacks[1]["nom"]   ?>
             <ul>
         <nav>
-
-            
+        
     </body>
+    
 </html>
 

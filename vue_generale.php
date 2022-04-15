70e6e3b4aa0bef805a7ee7a8511a91c1965f1e3a
Anais <Anais@LAPTOP-TE767Q1J>
2022-04-15 10:52:33 +0200

    Anais/implementation bloc sncak dans géneral

--- vue_generale.php
+++ vue_generale.php
@@ -1,20 +1,26 @@
 <!DOCTYPE html>
 
+<?php
+//On se connecte a la bdd, et on recupere les differentes variables
+include 'conn.php';
+?>
+
+
 <html>
     <head>
         <meta charset="UTF-8">
         <title>TaverneBDJ</title>
-            <link rel="stylesheet" href="style.css"/>
+        <link rel="stylesheet" href="style.css"/>
     </head>
-    
+
     <body>
-        
+
         <!-- insérer le code pour la bannière -->
-        
+
         <p class = "intro"> Introduction </p>
-        
+
         <p> insérer blabla pour l'introduction </p>
-        
+
         <!-- 3 BOUTONS -->
         <nav class="menu-nav"> <!--création d'un menu navigation-->
             <ul> <!--//Stocker les differentes puces dans une balise-->
@@ -37,37 +43,48 @@
         </nav>
 
         <!-- 1ere section : snacks -->
-        </div>
-        <div id="snacks">
-            <h1 class = "snack">
-                SNACKS
-            </h1>
-            <p>
-                Voici tous les snacks que nous vous proposons :
-            </p>
-        </div>
-        
-        <!-- 2eme section : matériel -->
-        <div id="matos">
-            <h1 class = "matos">
-                MATERIEL
-            </h1>
-            <p>
-                Voici tout le matériel que nous vous proposons :
-            </p>
-        </div>
-        
-        <!-- 3eme section : location jeux de société -->
-        <div id="jeux">
-            <h1 class = "jeux">
-                JEUX
-            </h3>
-            <p>
-                Voici tous les jeux que nous vous proposons :
-            </p>
-        </div>
-        
-        <!-- 
-        <?php
-        ?>
-    </body>
+    </div>
+    <div id="snacks">
+        <h1 class = "snack">
+            SNACKS
+        </h1>
+        <p>
+            Voici tous les snacks que nous vous proposons :
+
+        <ul class="snacks_objet">
+            <?php
+            for ($i = 0; $i < sizeof($snacks); $i++) {
+                echo "<li class='snacks_list'><img src ='" . $snacks[$i]['lien_img'] . "' width='80px' 'class='img'>" . ""
+                . "<div class='snacks_elements' >" . $snacks[$i]["nom"] . ""
+                . "</div> prix </li>";
+            }
+            ?>
+        </ul> 
+
+    </p>
+</div>
+
+<!-- 2eme section : matériel -->
+<div id="matos">
+    <h1 class = "matos">
+        MATERIEL
+    </h1>
+    <p>
+        Voici tout le matériel que nous vous proposons :
+    </p>
+</div>
+
+<!-- 3eme section : location jeux de société -->
+<div id="jeux">
+    <h1 class = "jeux">
+        JEUX
+        </h3>
+        <p>
+            Voici tous les jeux que nous vous proposons :
+        </p>
+</div>
+
+<!-- 
+<?php
+?>
+</body>

c5c72bf71220258747f7ab65de975b70b902f0f9
Le Gall Victor <victor@looping.org>
2022-04-28 15:11:37 +0200

    ajouter valeur via un form a la bdd

--- tests/victor.php
+++ tests/victor.php
@@ -8,6 +8,12 @@
     </head>
     <body>
 
+    <?php
+    /*
+    Form pour rajouter un snack. pour le Snack, on a besoins du nom et de l'image.
+    l'id et la dispo sont definis par defaut (la dispo est a 0 de base. elle passe a 1 si il y a du stock)
+    */
+    ?>
     <form method="post">
         <input type = "text" name="snack_name" placeholder="Non du snack" required><br/>
         <input type = "text" name="snack_img" placeholder="lien image" required>
@@ -15,22 +21,36 @@
     </form>
 
     <?php
-    //On se connecte a la bdd
-    include '../database.php';
-    global $db;
-    
-    $q = $db -> query("SELECT * FROM snacks");
-    while ($snacks = $q -> fetch()) {
-        ?>
-        <li>
-            <a href="profil.php?q = " <?= $snacks['nom'];?>>
-                
-            </a>
-        </li>
+        /*On se connecte a la bdd
+        !!! Etape Nécessaire à TOUTES les pages ou on a besoins d'utiliser la BDD
+        */
+        include '../database.php';
+        global $db;
 
-    <?php
-    }
-    
+        //Une fois qu'on valide le Form, on effectue cette action
+        if (isset($_POST['formsend'])){ //Si on valide le form
+
+            //On extrait les variables du form. dans ce cas, on retrouve 2 variables, $snack_name et $snack_img (se sont les "name" dans le post)
+            extract($_POST);
+
+            //On verifie que des valeurs ont bien été rentrées
+            if (!empty($snack_name) && !empty($snack_img)){
+                
+                //Echo des variables pour verifier visuellement
+                echo $snack_name . '<br/>' . $snack_img;
+            
+                
+                //Inserer des données dans la BDD
+                //On prepaer la requette. Ici, on veut inserer un nom et une img dans le snack. On leur insere les valeurs associées aux variables 'nom' et 'img' (methode securisée)
+                $q = $db -> prepare("INSERT INTO snacks(nom, lien_img)  VALUES(:nom, :img)");
+                //On execute la requette en attribuant aux variables 'nom' et 'img' les variables du Form
+                $q-> execute([
+                    'nom' => $snack_name,
+                    'img' => $snack_img
+                ]);
+                
+            }
+        }
     ?>
 
     <body>

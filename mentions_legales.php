39b1f8dcb5387f744b6139bfc033072832288263
ASUS <ASUS@10.3.61.44>
2022-05-06 13:10:12 +0200

    Mentions légales 2 Amélie

--- mentions_legales.php
+++ mentions_legales.php
@@ -1,48 +1,84 @@
 <!DOCTYPE html>
 
 <html>
+    <style>
+        h1.banniere {
+            color: white;
+            text-align: center;
+            background-color: black;
+            font-weight: 900;
+            font-variant : small-caps;
+            padding: 20px;
+        }
+
+       p.date_mentions {
+            text-align: center;
+            font-style: italic;
+        } 
+        
+        footer{
+            background-color: black;
+            color: white;
+            font-style: italic;
+            padding: 20px;
+        }
+        
+        p.mentions {
+            padding: 10px;
+            margin-left: 30px;
+        }
+
+        h3.article{
+            font-variant : small-caps;
+            background-color: grey;
+            
+        }
+        
+    </style>
     <head>
         <meta charset="UTF-8">
         <title>TaverneBDJ</title>
-        <link rel="stylesheet" href="styles.css"/>
+        <!--<link rel="stylesheet" href="styles.css"/>-->
     </head>
 
     <body>
-        Mentions légales <br><br>
-        En vigueur au 06/05/2022<br><br>
+        <h1 class="banniere">Mentions légales</h1> 
+        <p class="date_mentions">En vigueur au 06/05/2022</p><br>
 
-        Conformément aux dispositions des Articles 6-III et 19 de la Loi n°2004-575 du 21 juin 2004 pour la 
-        Confiance dans l’économie numérique, dite L.C.E.N., il est porté à la connaissance des utilisateurs et visiteurs, ci-après l""Utilisateur", du site www.taverneBDJ.fr , ci-après le "Site", les présentes mentions légales.<br><br>
+        <p class="mentions">Conformément aux dispositions des Articles 6-III et 19 de la Loi n°2004-575 du 21 juin 2004 pour la 
+        Confiance dans l’économie numérique, dite L.C.E.N., il est porté à la connaissance des utilisateurs et visiteurs,
+        ci-après l""Utilisateur", du site www.taverneBDJ.fr , ci-après le "Site", les présentes mentions légales.</p>
 
-        La connexion et la navigation sur le Site par l’Utilisateur implique acceptation intégrale et 
-        sans réserve des présentes mentions légales.<br><br>
+        <p class="mentions">La connexion et la navigation sur le Site par l’Utilisateur implique acceptation intégrale et 
+        sans réserve des présentes mentions légales.</p>
 
-        Ces dernières sont accessibles sur le Site à la rubrique « Mentions légales ».<br><br>
+        ARTICLE 1 - L'EDITEUR
 
-        ARTICLE 1 - L'EDITEUR<br><br>
+        <p class="mentions">L’édition et la direction de la publication du Site est assurée par BDJ EPF Montpellier, domiciliée 21 bd. 
+        Berthelot.</p>
+        
+        ARTICLE 2 - L'HEBERGEUR
 
-        L’édition et la direction de la publication du Site est assurée par BDJ EPF Montpellier, domiciliée 21 bd. Berthelot.<br><br>
+        <p class="mentions">L'hébergeur du Site est la société _______________, dont le siège social est situé au _______________ , 
+        avec le numéro de téléphone : _______________ + adresse mail de contact</p>
 
-        ARTICLE 2 - L'HEBERGEUR<br><br>
+        ARTICLE 3 - ACCES AU SITE
 
-        L'hébergeur du Site est la société _______________, dont le siège social est situé au _______________ , 
-        avec le numéro de téléphone : _______________ + adresse mail de contact<br><br>
+        <p class="mentions">Le Site est accessible en tout endroit, 7j/7, 24h/24 sauf cas de force majeure, interruption programmée ou non 
+            et pouvant découlant d’une nécessité de maintenance. <br><br>
 
-        ARTICLE 3 - ACCES AU SITE<br><br>
+        En cas de modification, interruption ou suspension du Site, l'Editeur ne saurait être tenu responsable.</p>
 
-        Le Site est accessible en tout endroit, 7j/7, 24h/24 sauf cas de force majeure, interruption programmée ou non et pouvant découlant d’une nécessité de maintenance.<br><br>
+        ARTICLE 4 - COLLECTE DES DONNEES
 
-        En cas de modification, interruption ou suspension du Site, l'Editeur ne saurait être tenu responsable.<br><br>
-
-        ARTICLE 4 - COLLECTE DES DONNEES<br><br>
-
-        Le site est exempté de déclaration à la Commission Nationale Informatique et Libertés (CNIL) dans la mesure où il ne collecte aucune donnée concernant les utilisateurs.
+        <p class="mentions">Le site est exempté de déclaration à la Commission Nationale Informatique et Libertés (CNIL) dans la mesure où 
+        il ne collecte aucune donnée concernant les utilisateurs.</p>
 
     </body>
 
     <footer>
-        <p>
-            Toute utilisation, reproduction, diffusion, commercialisation, modification de toute ou partie du Site, sans autorisation de l’Editeur est prohibée et pourra entraînée des actions et poursuites judiciaires telles que notamment prévues par le Code de la propriété intellectuelle et le Code civil.
-        </p>
+            Toute utilisation, reproduction, diffusion, commercialisation, modification de toute ou partie du Site, sans 
+            autorisation de l’Editeur est prohibée et pourra entraîner des actions et poursuites judiciaires telles que 
+            notamment prévues par le Code de la propriété intellectuelle et le Code civil.
     </footer>
 </html>
